ed77bd753adfaf4dadf82036d51d7276378d6cd9
nipil <nipil@users.noreply.github.com>
2016-10-19 21:48:10 +0200

    list of transfered file (*.list) + refactor

--- cron_log_rsync.sh
+++ cron_log_rsync.sh
@@ -5,14 +5,17 @@
 err() { >&2 echo $@; }
 log() { echo $@ >> ${LOG}; }
 cleanup() { rm -f ${TMP1} ${TMP2}; }
+fail() {
+	cleanup
+	# display message if provided
+	[[ -n ${1} ]] && err ${1};
+	# exit with provided status code, if any
+	[[ -n ${2} ]] && exit ${2} || exit 1;
+}
 
 ##############################################################################
 # test parameters
-if [[ -z $1 || -z $2 || -z $3 ]]
-then
-	err "Usage: $0 SYNC_ID RSYNC_SRC RSYNC_DST" 1>&2
-	exit 1
-fi
+[[ -n $1 && -n $2 && -n $3 ]] || fail "Usage: $0 SYNC_ID RSYNC_SRC RSYNC_DST"
 
 ##############################################################################
 # set variables
@@ -22,28 +25,19 @@ RSYNC_DST=${3}
 LOG=${0}.${1}.log
 LOCK=${0}.${1}.lock
 LAST=${0}.${1}.last
+ACT=${0}.${1}.list
 FLOCK_ERR=100
-TIMESTAMP_START=$(date +'%F %T')
 
 ##############################################################################
 # create temporary files
 TMP1=$(mktemp -p /tmp "tmp_$1.XXXXXXXXXX")
-RES=${?}
-if [ ${RES} -ne 0 ]
-then
-	err "cannot create temporary file"
-	exit 1
-fi
+[[ ${?} -eq 0 ]] || fail "cannot create temporary file"
 TMP2=$(mktemp -p /tmp "tmp_$1.XXXXXXXXXX")
-RES=${?}
-if [ ${RES} -ne 0 ]
-then
-	err "cannot create temporary file"
-	exit 1
-fi
+[[ ${?} -eq 0 ]] || fail "cannot create temporary file"
 
 ##############################################################################
 # do sync
+TIMESTAMP_START=$(date +'%F %T')
 flock -x -n -E ${FLOCK_ERR} ${LOCK} rsync -a -v ${RSYNC_SRC} ${RSYNC_DST} 2>${TMP2} 1>${TMP1}
 RES=${?}
 TIMESTAMP_END=$(date +'%F %T')
@@ -74,16 +68,16 @@ then
 	log "STDERR dump:"
 	cat ${TMP2} >> ${LOG}
 
-	# cron output
-	err ${MSG}
-
-	cleanup
-	exit ${RES}
+	# cron output and exit
+	fail ${MSG} ${RES}
 fi
 
 ##############################################################################
 # check activity
-ACTIVITY=$(cat ${TMP1} | head -n -3 | tail -n +2 | grep -v '^\./$' | wc -l)
+
+cat ${TMP1} | head -n -3 | tail -n +2 | grep -v '^\./$' > ${ACT}
+[[ ${?} -eq 0 ]] || fail "cannot create activity file"
+ACTIVITY=$(cat ${ACT} | wc -l)
 echo "${TIMESTAMP_END}: activity=${ACTIVITY}" > ${LAST}
 
 ##############################################################################
